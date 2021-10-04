c10a8786ff12b2296d595cd58275e69a5f343689
Phuc <phxc29@gmail.com>
2021-10-04 23:04:09 +0700

    after successful create, save this sentence to new group

---
+++ Bilinguals/Scripts/dropdown-sentence.js
@@ -0,0 +1,172 @@
+﻿
+
+// save and remove sentence
+var sentenceId;
+var handleSaveRemove = function () {
+    var _savingUrl = '/Sentences/SaveToMyFeaturedSentences?sentenceId=';
+    var _removingUrl = '/Sentences/RemoveFromMyFeaturedSentence?userSentenceId=';
+    var groupId;
+    var userSentenceId;
+
+    // https://stackoverflow.com/questions/29388002/jquery-ajax-sending-multiple-request-on-1-click
+
+    $('.btn-select').on('click', function (e) {
+        var _this = $(this);
+        sentenceId = _this.data('sentenceid'); //note: lower case
+        console.log(sentenceId);
+        userSentenceId = _this.attr('data-usersentenceid');
+
+        //save
+        $('.save').off('click').on('click', function (e) {
+            groupId = $(this).data('groupid');
+            var url = _savingUrl + sentenceId + '&groupId=' + groupId;
+
+            $.ajax({
+                url: url,
+                success: function (data) {
+                    _this.find('#gn').remove();
+                    _this.removeClass('btn-outline-primary').addClass('btn-primary')
+                        .attr('data-usersentenceid', data.userSentenceId)
+                        .append(`<span id="gn">${data.groupName}</span>`)
+                        .find('.bi').removeClass('bi-bookmark-plus').addClass('bi-bookmark-check');
+                }
+            });
+
+        });
+
+        //remove
+        $('.remove').off('click').on('click', function (e) {
+            var URl = _removingUrl + userSentenceId;
+
+            $.ajax({
+                url: URl,
+                success: function () {
+                    _this.find('#gn').remove();
+                    _this.removeClass('btn-primary').addClass('btn-outline-primary')
+                        .attr('data-usersentenceid', "")
+                        .find('.bi').removeClass('bi-bookmark-check').addClass('bi-bookmark-plus');
+                },
+                error: function () {
+                    //thong bao chua luu 
+                    console.log("chua luu remove cai quan a");
+                },
+
+            });
+        });
+    });
+};
+
+// 
+var simplePromiseRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: url,
+            success: (data) => {
+                resolve(data);
+            },
+            error: (res) => {
+                reject(res);
+            },
+        });
+    });
+}
+
+// get list groups 
+var groupService = (function () {
+
+    var _resourceUrl = '/groups/getusergroups';
+    var getUserGroups = () => {
+        return simplePromiseRequest(_resourceUrl);
+    };
+
+    return { getUserGroups, }
+})();
+
+//json request group list
+var getUserGroups = function () {
+    var _group = [];
+    groupService.getUserGroups().then((data) => {
+        console.log(data);
+        _group = data;
+        var rows = _group.reduce((all, group) => {
+            var groupRow = `<li><a data-groupid="${group.id}" class="dropdown-item save" href="#">${group.name}</a></li>`;
+            return all += groupRow;
+        }, '');
+        $('.save').remove();
+        $('.dropdown-menu').append(rows);
+    });
+}
+
+// handle create group (promise calback)
+var bsModal = (function () {
+    var _completeCallback, _beforeShowCallback;
+    
+    var getGroup = () => { return simplePromiseRequest('/groups/create'); }
+
+    function handleBsModal() {
+
+        
+
+        $('#bsModal').on('show.bs.modal', function () {
+            _beforeShowCallback();
+        });
+
+        $('#bsModal').on('shown.bs.modal', function (event) {
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var modal = $(this);
+            
+            getGroup()
+                .then((data) => {
+                    modal.find('.modal-content').empty().append(data); //data = partial view
+                })
+                .then(bindAjaxForm) //ajaxForm request submit here
+                .then((data) => {
+                    _completeCallback(data);
+                    // after successful create, save this sentence to new group
+                    saveSentence(data.id);
+                });
+        });
+    }
+
+    function bindAjaxForm() {
+        var myForm = $('#bsModal').find('form');
+        
+        return new Promise((resolve, reject) => {
+            myForm.ajaxForm({
+                beforeSubmit: function (data) {
+                },
+                success: function (data) {
+                    $('#bsModal').find('button.btn-close').trigger('click');
+                    resolve(data);
+                },
+                error: reject
+            });
+        });
+    } 
+
+    function saveSentence(groupId) {
+        var _this = $("div").find(`[data-sentenceid="${sentenceId}"]`); ///https://stackoverflow.com/questions/4191386/jquery-how-to-find-an-element-based-on-a-data-attribute-value
+        var url = '/Sentences/SaveToMyFeaturedSentences?sentenceId=' + sentenceId + '&groupId=' + groupId;
+        $.ajax({
+            url: url,
+            success: function (data) {
+                _this.find('#gn').remove();
+                _this.removeClass('btn-outline-primary').addClass('btn-primary')
+                    .attr('data-usersentenceid', data.userSentenceId)
+                    .append(`<span id="gn">${data.groupName}</span>`)
+                    .find('.bi').removeClass('bi-bookmark-plus').addClass('bi-bookmark-check');
+            },
+        });
+    } 
+
+    return {
+        init: function (beforeShowCallback, completeCallback) {
+            handleBsModal();
+            _completeCallback = completeCallback;
+            _beforeShowCallback = beforeShowCallback;
+        }
+        ,
+        setState: function () { }
+    };
+})();
